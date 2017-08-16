eced314c0ad8a862903468d106c3204150980df0
lglayat <glayatle@g.cofc.edu>
2017-08-16 14:31:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  # resources :products
 
-  # You can have the root of your site routed with "root"
+  get '/' => 'products#index'
+  post '/' => 'products#add'  # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
